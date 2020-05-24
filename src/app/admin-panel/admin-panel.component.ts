c4447983905e968cf33d017bbaf7544dffac0568
cosmacatalin98 <cosmacatalin98@yahoo.com>
2020-05-24 15:00:55 +0300

    Implement product administration

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { ProductService } from "../product.service";
+import { UserService } from "../user.service";
+import { Product } from "../product";
 
 @Component({
-  selector: 'app-admin-panel',
-  templateUrl: './admin-panel.component.html',
-  styleUrls: ['./admin-panel.component.css']
+  selector: "app-admin-panel",
+  templateUrl: "./admin-panel.component.html",
+  styleUrls: ["./admin-panel.component.css"]
 })
 export class AdminPanelComponent implements OnInit {
+  products = [];
 
-  constructor() { }
+  pId = new FormControl("");
+  pSId = new FormControl("");
+  pName = new FormControl("");
+  pQuantity = new FormControl("");
+  pUm = new FormControl("");
+  pPrice = new FormControl("");
+
+  pIdDelete = new FormControl("");
+
+
+  prd: Product;
+  prd1: Product;
+
+  constructor(
+    private productService: ProductService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
+    this.productService
+      .getProducts("")
+      .subscribe(data => (this.products = data));
   }
 
+  getAllProducts() {
+    this.productService
+      .getProducts("all")
+      .subscribe(data => (this.products = data));
+  }
+
+  insertNewProduct() {
+    this.prd = {
+      productId: this.pId.value,
+      storeId: this.pSId.value,
+      productName: this.pName.value,
+      um: this.pUm.value,
+      price: this.pPrice.value,
+      quantity: this.pQuantity.value
+    };
+
+    this.productService
+      .insertProduct(this.prd)
+      .subscribe(data => (this.prd1 = data));
+  }
+
+  updateProduct() {
+    this.prd = {
+      productId: this.pId.value,
+      storeId: this.pSId.value,
+      productName: this.pName.value,
+      um: this.pUm.value,
+      price: this.pPrice.value,
+      quantity: this.pQuantity.value
+    };
+
+    this.productService
+      .updateProduct(this.prd)
+      .subscribe(data => (this.prd1 = data));
+  }
+
+  deleteProduct(){
+    this.productService.deleteProduct(this.pIdDelete.value).subscribe();
+  }
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,25 +1,43 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { Product } from './product';
-import {HttpParams} from "@angular/common/http";
-import {HttpHeaders} from "@angular/common/http";
-import { catchError, map, tap } from 'rxjs/operators';
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { Product } from "./product";
+import { HttpParams } from "@angular/common/http";
+import { HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable()
 export class ProductService {
+  private productUrl = "http://localhost:8080/findByName/";
+  private insertProductUrl = "http://localhost:8080/insertNewProduct";
+  private updateProductUrl = "http://localhost:8080/updateProduct";
+  private deleteProductUrl = "http://localhost:8080/deleteProduct";
 
-  private productUrl = 'http://localhost:8080/findByName/';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  getProducts(name: string): Observable<Product[]> {
+    if (name == "all") {
+      return this.http.get<Product[]>("http://localhost:8080/allProducts");
+    }
 
-  getProducts(name : string):Observable<Product[]>{
-  
-    const options = 
-   { params: new HttpParams().set('name', name) };
+    const options = { params: new HttpParams().set("name", name) };
 
     return this.http.get<Product[]>(this.productUrl, options);
   }
 
+  insertProduct(product: Product): Observable<Product> {
+    const options = { responseType: "text" };
+    return this.http.post<Product>(this.insertProductUrl, product, options);
+  }
+
+  updateProduct(product: Product): Observable<Product> {
+    const options = { responseType: "text" };
+    return this.http.put<Product>(this.updateProductUrl, product, options);
+  }
+
+  deleteProduct(id: number): Observable<{}> {
+    const options = { responseType: "text" };
+    const url = `${this.deleteProductUrl}/${id}`;
+    return this.http.delete<Product>(url, options);
+  }
 }
